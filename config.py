ada87251b15fe09e9399d3791a0b6a41d4975ac3
Alexander Schumacher <aschuma5@emich.edu>
2020-10-17 23:41:15 +0000

    Modifying config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 data_keys = dict(
-                api_key = "AIzaSyCw4PJQ81FLtSkl-dQdoaPBnCmHHW2pO2g" ,#INSERT your API key in the brackets for later use in the programs
-                cse_key = "e614e3f79d9c3a3fd", #INSERT YOUR CSE KEY
-                my_search_topic = "40k God Emperor" #Search Term
+                api_key = "" ,#INSERT your API key in the brackets for later use in the programs
+                cse_key = "", #INSERT YOUR CSE KEY
+                my_search_topic = "" #Search Term
                 )
 
