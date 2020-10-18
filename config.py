bc9dad4eb2017535a0fe4350a8dfa7005bf7f6cd
Alexander Schumacher <aschuma5@emich.edu>
2020-10-18 00:08:42 +0000

    Checking if ignore works:

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 data_keys = dict(
-                api_key = "" ,#INSERT your API key in the brackets for later use in the programs
-                cse_key = "", #INSERT YOUR CSE KEY
-                my_search_topic = "" #Search Term
+                api_key = "" ,#INSERT your API key in the brackets for later use in the programs in quotation marks
+                cse_key = "", #INSERT YOUR CSE KEY in quotation marks
+                my_search_topic = "" #Search Term shoud be placed in the quotation marks
                 )
 
