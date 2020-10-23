da8cbc75395ab728b4dbea89252360ecae9b7a52
Alexander Schumacher <aschuma5@emich.edu>
2020-10-23 20:05:58 +0000

    Modified file download_youtube_data.py

--- download_youtube_data.py
+++ download_youtube_data.py
@@ -7,11 +7,6 @@ import config
 
 
 my_api_key = config.data_keys['api_key']
-#my_cse_id = config.data_keys['cse_key']
-
-#my_api_key = 'AIzaSyCw4PJQ81FLtSkl-dQdoaPBnCmHHW2pO2g'
-
-#my_cse_id = 'e614e3f79d9c3a3fd'
 
 
 
