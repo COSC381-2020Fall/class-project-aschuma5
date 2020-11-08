e014e10348ac52cb072e191a7d0b505ea950fb2a
aschuma5 <70524492+aschuma5@users.noreply.github.com>
2020-11-08 18:47:19 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-api_key = 'AIzaSyCw4PJQ81FLtSkl-dQdoaPBnCmHHW2pO2g'
-cse_id = 'e614e3f79d9c3a3fd' 
+api_key = 'api_key'
+cse_id = 'cse_id'
 
 search_term = 'Warhammer 40k'
-google_search_maximum_page_number = 1
+google_search_maximum_page_number = 10
+
+gmail_password = ""
